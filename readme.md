8900554cb2d7d8a9b3464da48494378f07e9d2a2
Alex Buznik <shu@buznik.net>
2020-02-05 12:58:54 +0200

    Some colors in the stdout

--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 Simple traffic light program.
 
 Prototype to be run on ESP32 or other Arduino.
+
+
+# Dependencies
+
+colorama
--- traffic_light.py
+++ traffic_light.py
@@ -1,20 +1,21 @@
 # traffic_light.py
 import time
-import sys
+from colorama import init, Fore, Back, Style
 
 currentTime = 0
-targetTime = time.time()
+targetTime = time.time() - 1
 currentPositionInPattern = 0
 colorPosition = 0
 currentState = [ False, False, False ]
-basicPattern = [ 5, 0, 0.5, 0.5, 0.5, 0, 5, 0 ]
+basicPattern = [ 5, 0.3, 0.3, 0, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0, 5, 0 ]
 COLORS = [ 'red', 'yellow', 'green' ]
 
 currentPattern = basicPattern
 currentState[colorPosition] = True
-targetTime = targetTime + currentPattern[currentPositionInPattern]
-
 done = False
+
+init()
+
 while not done:
   currentTime = time.time()
   if currentTime > targetTime:
@@ -36,5 +37,30 @@ while not done:
 
     targetTime = targetTime + nextTarget
 
-    print COLORS[colorPosition] + str(currentState[colorPosition] )
+    red = Fore.RED
+    if colorPosition is 0 and currentState[colorPosition]:
+      red = red + Back.RED
+    else:
+      red = red + Back.BLACK
 
+    red = red + 'red' + Style.RESET_ALL
+
+
+    yellow = red + Fore.YELLOW
+    if colorPosition is 1 and currentState[colorPosition]:
+      yellow = yellow + Back.YELLOW
+    else:
+      yellow = yellow + Back.BLACK
+
+    yellow = yellow + 'yellow' + Style.RESET_ALL
+
+
+    green = yellow + Fore.GREEN
+    if colorPosition is 2 and currentState[colorPosition]:
+      green = green + Back.GREEN
+    else:
+      green = green + Back.BLACK
+
+    green = green + 'green' + Style.RESET_ALL
+
+    print(green)
